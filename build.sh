2637bf1635a3f6dc32682743fb7e8aa617ecf0cc
Paul Moore <p.f.moore@gmail.com>
2019-12-07 15:49:25 +0000

    Add wildcard support

--- build.sh
+++ build.sh
@@ -7,8 +7,12 @@ wget http://ftp.gnu.org/gnu/diffutils/diffutils-${DIFF_VER}.tar.xz
 tar xf diffutils-${DIFF_VER}.tar.xz
 cd diffutils-${DIFF_VER}
 
+# Wildcard expansion
+echo "int _dowildcard = -1;" >wild.c
+x86_64-w64-mingw32-gcc -c wild.c
+
 # Static to avoid depending on libwinpthread-1.dll at runtime
-mingw64-configure --disable-nls LDFLAGS="-static" CFLAGS="-DSIGSTOP=SIGINT -DSIGTSTP=SIGINT -DSIGALRM=SIGINT -DSIGHUP=SIGINT -DSIGQUIT=SIGINT -DSIGPIPE=SIGINT"
+mingw64-configure --disable-nls LDFLAGS="-static" LIBS="$(pwd)/wild.o" CFLAGS="-DSIGSTOP=SIGINT -DSIGTSTP=SIGINT -DSIGALRM=SIGINT -DSIGHUP=SIGINT -DSIGQUIT=SIGINT -DSIGPIPE=SIGINT"
 
 # Hack around error in gnulib-tests (which doesn't matter)
 mv gnulib-tests gnulib-tests#
@@ -16,6 +20,7 @@ mkdir gnulib-tests
 echo all: >gnulib-tests/Makefile
 
 make
+x86_64-w64-mingw32-strip -s */*.exe
 
 TARGET=../diffutils-${DIFF_VER}.zip
 zip -j9 ${TARGET} */*.exe
